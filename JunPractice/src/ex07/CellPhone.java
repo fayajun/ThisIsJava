627ff9acde5562bf1a26ae8118e6b97bccd23b4d
fayajun <fayajun@gmail.com>
2021-01-10 16:54:58 +0900

    CellPhonePractice

---
+++ JunPractice/src/ex07/CellPhone.java
@@ -0,0 +1,18 @@
+package ex07;
+
+public class CellPhone {
+	//fields
+	String model;
+	String color;
+	
+	//constructor
+	
+	//methods
+	void powerOn() {System.out.println("전원을 컵니다");}
+	void powerOff() {System.out.println("전원을 끕니다");}
+	void bell() {System.out.println("벨이 울립니다");}
+	void sendVoice(String message) {System.out.println("자기: " + message);}
+	void receiveVoice(String message) {System.out.println("상대방: " + message);}
+	void hangUp() {System.out.println("전원을 끊습니다");}
+	
+}
